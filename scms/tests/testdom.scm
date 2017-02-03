ff11f163d1631475397c8f93a8843fdd13a28588
hlpp <1013644379@qq.com>
2017-02-03 17:52:08 +0800

    update

--- scms/tests/testdom.scm
+++ scms/tests/testdom.scm
@@ -1,3 +1,6 @@
+﻿;;使方向键控制按钮
+
+
 (define (addpx d a n)
   (define (getpx str)
     (string->number str))
@@ -20,3 +23,29 @@
           ((= key KEY_D) (addpx d 'top +10))
           ((= key KEY_L) (addpx d 'left -10))
           ((= key KEY_R) (addpx d 'left +10)))))
+
+
+
+;;让按钮跟随鼠标，按esc键切换暂停/开始
+(define (topxstr num)
+  (string-append (number->string num) "px"))
+
+(define e (getElementById document "run"))
+(define d (dom-get e 'style))
+(dom-set d 'position "absolute")
+(dom-set d 'left "100px")
+(dom-set d 'top "200px")
+
+(define pause #f)
+
+(dom-set-event document 'onmousemove
+  (lambda (event)
+    (if (not pause)
+      (begin
+        (dom-set d 'left (topxstr (dom-get event 'clientX)))
+        (dom-set d 'top (topxstr (dom-get event 'clientY)))))))
+
+(dom-set-event document 'onkeydown
+  (lambda (event)
+    (if (= (dom-get event 'keyCode) 27)
+        (set! pause (not pause)))))
