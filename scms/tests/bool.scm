4401f358475fdba2608c52938d78e0f5959a9fc9
hlpp <1013644379@qq.com>
2017-02-09 18:08:26 +0800

    add tests

--- scms/tests/bool.scm
+++ scms/tests/bool.scm
@@ -1,7 +1,15 @@
+(boolean? #t);#t
+(boolean? #f);#t
+(boolean? (> 1 2));#t
+(boolean? 6);#f
+(boolean? '());#f
+(boolean? (boolean? *));#t
 (not #t);#f
 (not #f);#t
+(not '());#f
 (not 1);#f
 (not 0);#f
 (not 1.2);#f
 (not "");#f
 (not "s");#f
+(not +);#f
--- scms/tests/test1.scm
+++ scms/tests/test1.scm
@@ -58,22 +58,3 @@ x
     (newline)))
 	
 
-(define x 5) ;定义变量x
-
-;定义变量l,值为一个表，有3个元素:符号+,2,符号x
-;'(+ 2 x)是(quote (+ 2 x))的简写
-;quote的功能是返回表达式本身，即一个列表，而不进行意义的求值
-(define l '(+ 2 x))
-
-;;调用eval
-;接收scheme中程序和表达式的数据表示以及一个环境，在此环境中求值
-;(interaction-environment)返回全局环境
-(eval l (interaction-environment))
-
-;;调用appply
-;第一个参数必须为一个过程((procedure? x)为真的参数)
-;以及该过程需要的实际参数组成的列表
-(apply (eval (car l) (interaction-environment))
-       (list 2 5))
-
-
