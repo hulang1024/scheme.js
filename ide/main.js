ebff87efdc4931eb737e6e549c63b39e26e91768
hlpp <1013644379@qq.com>
2017-02-21 13:26:46 +0800

    Add files via upload

--- ide/main.js
+++ ide/main.js
@@ -11,50 +11,51 @@ var schemeConsole;
 var definitionEditor;
 
 window.onload = function(){
-    loadSchemeKernelJS(function(){
-        schemeConsole = new Console();
-        divConsole = schemeConsole.divConsole;
-        divDefinitions = document.getElementById('definitions');
-        textareaDefinitions = document.getElementById('definitions_textarea');
-        btnViewPlain = document.getElementById("viewPlain");
-        btnToggleDefinitions = document.getElementById('toggleDefinitions');
-        btnToggleInteractions = document.getElementById('toggleInteractions');
-        btnConsoleClear = document.getElementById('clearConsole');
-        
-        definitionEditor = CodeMirror.fromTextArea(textareaDefinitions, {
-            lineNumbers: true,
-            matchBrackets: true,
-        });
-        definitionEditor.setSize('height', "100%");
-        definitionEditor.setOption('theme', "scheme-classic-color");
-        
-        btnRun = document.getElementById('run');
-        contentHeight = window.innerHeight ||
-            document.documentElement.clientHeight || document.body.clientHeight;
-        var content = document.getElementById("content");
-        content.style.height = contentHeight + "px";
-        divDefinitions.style.display = "block";
-        definitionsHeight = contentHeight * 0.5;
-        consoleHeight = contentHeight - definitionsHeight;
-        divDefinitions.style.height = definitionsHeight + "px";
-        divConsole.style.height = (contentHeight - definitionsHeight) + "px";
-        
-        btnViewPlain.onclick = function() {
-            var win = window.open("", document.title + " - Plain Code");
-            var pcode = document.createElement('pre');
-            pcode.innerText = definitionEditor.getValue();
-            win.document.body.appendChild(pcode);
-        }
-        btnToggleDefinitions.onclick = toggleDefinitions;
-        btnToggleInteractions.onclick = toggleInteractions;
-        btnConsoleClear.onclick = function() {
-            schemeConsole.clear();
-            schemeConsole.resetInput();
-        }
-        btnRun.onclick = run;
-        
-        scheme.console = schemeConsole;
+loadSchemeKernelJS(function(){
+    schemeConsole = new Console();
+    divConsole = schemeConsole.divConsole;
+    divDefinitions = document.getElementById('definitions');
+    textareaDefinitions = document.getElementById('definitions_textarea');
+    btnViewPlain = document.getElementById("viewPlain");
+    btnToggleDefinitions = document.getElementById('toggleDefinitions');
+    btnToggleInteractions = document.getElementById('toggleInteractions');
+    btnConsoleClear = document.getElementById('clearConsole');
+    
+    definitionEditor = CodeMirror.fromTextArea(textareaDefinitions, {
+        lineNumbers: true,
+        matchBrackets: true,
     });
+    definitionEditor.setSize('height', "100%");
+    definitionEditor.setOption('theme', "scheme-classic-color");
+    
+    btnRun = document.getElementById('run');
+    contentHeight = window.innerHeight ||
+        document.documentElement.clientHeight || document.body.clientHeight;
+    var content = document.getElementById("content");
+    content.style.height = contentHeight + "px";
+    divDefinitions.style.display = "block";
+    definitionsHeight = contentHeight * 0.5;
+    consoleHeight = contentHeight - definitionsHeight;
+    divDefinitions.style.height = definitionsHeight + "px";
+    divConsole.style.height = (contentHeight - definitionsHeight) + "px";
+    
+    btnViewPlain.onclick = function() {
+        var win = window.open("", document.title + " - Plain Code");
+        var pcode = document.createElement('pre');
+        pcode.innerText = definitionEditor.getValue();
+        win.document.body.appendChild(pcode);
+    }
+    btnToggleDefinitions.onclick = toggleDefinitions;
+    btnToggleInteractions.onclick = toggleInteractions;
+    btnConsoleClear.onclick = function() {
+        schemeConsole.clear();
+        schemeConsole.resetInput();
+    }
+    btnRun.onclick = run;
+    
+    scheme.console = schemeConsole;
+});
+
 }
 
 function toggleDefinitions() {
@@ -100,32 +101,4 @@ function run() {
    
     scheme.evalString(definitionEditor.getValue());
     schemeConsole.resetInput();
-}
-
-function loadSchemeKernelJS(after) {
-    loadJSSeq([
-        "object", "read", "print", "env", "error", "bool",
-        "symbol", "number", "char", "string", "list", "vector", "myobject", "fun", "syntax",
-        "lib/client-javascript-lib", "eval", "lib/alib.scm"],
-        function(){
-            after();
-        });
-
-    function loadJSSeq(jsseq, after) {
-        loadNextJS(0);
-        
-        function loadNextJS(index) {
-            if(index > jsseq.length - 1) {
-                after();
-                return;
-            }
-        
-            var s = document.createElement("script");
-            s.src = jsseq[index] + ".js?v=" + new Date().getTime();
-            s.onload = function() {
-                loadNextJS(index + 1);
-            }
-            document.body.appendChild(s);
-        }
-    }
 }
