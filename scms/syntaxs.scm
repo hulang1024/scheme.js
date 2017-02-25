2994c790354fc54de97c8b186a041af6dd007d4f
hlpp <1013644379@qq.com>
2017-02-25 20:51:13 +0800

    宏笔记,目前不支持宏机制

---
+++ scms/syntaxs.scm
@@ -0,0 +1,59 @@
+(define-syntax when
+  (syntax-rules ()
+    ((_ test body ...)
+     (if test
+         (begin body ...)))))
+
+(define-syntax unless
+  (syntax-rules ()
+   ((_ test body ...)
+     (if (not test)
+         (begin body ...)))))
+
+(define-syntax or
+  (syntax-rules ()
+    ((or) #f)
+    ((or e) e)
+    ((or e1 e2 ...)
+     (let ((temp e1))
+       (if temp
+           temp
+           (or e2 ...))))))
+
+(define-syntax do
+  (syntax-rules ()
+    ((do ((variable1 init1 step1) ...)
+         (test expression ...)
+         command ...)
+     (let iter ((variable1 init1))
+         (if test
+             (begin expression ...)
+             (begin command ...
+                    (iter step1 ...)))))))
+
+;C like style's while
+(define-syntax while
+  (syntax-rules ()
+    ((_ test body ...)
+     (let loop ()
+       (when test
+             body ...
+             (loop))))))
+
+(define-syntax for
+  (syntax-rules (to)
+    ;; loop in sequence
+    ;; (for i (0 to 10) do something...)
+    ((_ i in (from to end) body ...)
+     (let loop ((i from))
+       (when (< i end)
+             body ...
+             (loop (+ i 1)))))
+    ;; loop in list
+    ;; (for i in '(a b c) do something...)
+    ((_ i in lst body ...)
+     (let loop ((l lst))
+       (unless (null? l)
+               (let ((i (car l)))
+                 body ...
+                 (loop (cdr l))))))))
