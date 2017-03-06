51c1da7088f8e79004189e392e5be1817dfebfae
hulang <1013644379@qq.com>
2017-03-06 17:57:04 +0800

    add scheme scripts scan

---
+++ jsscheme/scm.js
@@ -0,0 +1,15 @@
+﻿loadScheme("../", function(){
+    var scmScripts = scanSchemeScripts();
+    var ss;
+    for (var i = 0; i < scmScripts.length; i++) {
+        ss = scmScripts[i];
+        if(!ss.hasAttribute("ignore")) {
+            scheme.evalStringWithEnv(ss.innerText, scheme.globalEnvironment);
+        }
+    }
+    
+    function scanSchemeScripts() {
+        return document.querySelectorAll("script[type='text/scm']");
+    }
+});
+
