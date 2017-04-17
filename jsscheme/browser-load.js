ebf720ffe789a225854c694f5d0c5f602f86bb15
problue <1013644379@qq.com>
2017-04-17 20:04:02 +0800

    fix $LAB is undefined

--- jsscheme/browser-load.js
+++ jsscheme/browser-load.js
@@ -1,34 +1,46 @@
-﻿var loadScheme = function(rootdir, after) {
-	var jsArr = [
-        "base/object",
-        "base/read",
-        "base/print",
-        "base/error",
-        "base/bool",
-        "base/symbol",
-        "base/number",
-        "base/numcomp",
-        "base/numarith",
-        "base/numstr",
-        "base/char",
-        "base/string",
-        "base/list",
-        "base/vector",
-        "base/fun",
-        "base/syntax",
-        "base/eval",
-        "lib/browserjs",
-        "base/env",
-        "lib/lib.scm"].map(function(s){
-			return (rootdir || "") + "./jsscheme/" + s + ".js?v=" + new Date().getMonth();
+﻿﻿var loadScheme = function(rootdir, after) {
+	rootdir = (rootdir || "");
+	
+	if(window['$LAB']) {
+	    load();
+	} else {
+		var s = document.createElement("script");
+		s.src = rootdir + "./lib/LAB.min.js";
+		s.onload = load;
+		document.body.appendChild(s);
+	}
+	
+	function load() {
+		var jsArr = [
+			"base/object",
+			"base/read",
+			"base/print",
+			"base/error",
+			"base/bool",
+			"base/symbol",
+			"base/number",
+			"base/numcomp",
+			"base/numarith",
+			"base/numstr",
+			"base/char",
+			"base/string",
+			"base/list",
+			"base/vector",
+			"base/fun",
+			"base/syntax",
+			"base/eval",
+			"lib/browserjs",
+			"base/env",
+			"lib/lib.scm"].map(function(s){
+				return rootdir + "./jsscheme/" + s + ".js?v=" + new Date().getMonth();
 		});
-	$LAB.setOptions({AlwaysPreserveOrder:true})
-	    .script(jsArr).wait(function(){
-        	$LAB.setOptions({AlwaysPreserveOrder:false});
-                //env
-                scheme.initBasicEnv();
-                scheme.makeGlobalEnv();
+	    $LAB.setOptions({AlwaysPreserveOrder:true})
+	        .script(jsArr).wait(function(){
+        	    $LAB.setOptions({AlwaysPreserveOrder:false});
+                    scheme.initBasicEnv();
+                    scheme.makeGlobalEnv();
             
-                after && after();
-            });
+                    after && after();
+                });
+	}
 }
