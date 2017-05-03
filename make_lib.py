7722dcf98246f99ae99630ca6d8569a3481476eb
problue <1013644379@qq.com>
2017-05-03 20:27:38 +0800

    Add make_lib.py

---
+++ make_lib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+import os
+
+SCM_IN_FILE  = 'jsscheme/lib/lib.scm'
+JS_OUT_FILE  = 'jsscheme/lib/lib.js'
+
+# Read the SCM file
+scmFile = open(SCM_IN_FILE, 'rb')
+scmSrc = scmFile.read().decode('utf-8')
+scmFile.close()
+
+jsSrc = 'var SCHEME_LIB_SRC = ' + repr(scmSrc) + ';'
+
+# Write the JS source
+jsFile = open(JS_OUT_FILE, 'w')
+jsFile.write(jsSrc)
+jsFile.close()
+
