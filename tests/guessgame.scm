a31423dd6d4f2c86afb731960bca21cba2cad211
hlpp <1013644379@qq.com>
2017-06-01 17:20:39 +0800

    -

---
+++ tests/guessgame.scm
@@ -0,0 +1,24 @@
+﻿;;; 猜数字
+;;; author: problue
+(define (promptnum msg)
+  (string->number (prompt msg)))
+(define times 0)
+(define (guess-loop)
+  (define g (promptnum "数"))
+  (set! times (+ times 1))
+  (let ((result #t))
+    (cond ((< g answer) (set! result "小了~"))
+          ((> g answer) (set! result "大了~"))
+          (else (alert (string-append "恭喜,你猜对了!尝试次数:"
+                                       (number->string times)))
+                (set! result #f)))
+    (if result
+      (if (confirm (string-append result "是否继续?"))
+          (guess-loop)))))
+(alert "欢迎进入猜数字游戏~")
+(define max (promptnum "请输入游戏的最大数:"))
+(if (> max 1)
+  (begin
+    (define answer (random-int 1 max))
+    (alert "ok，我已生成一个数。现在开始，请猜猜看")
+    (guess-loop)))
---
+++ tests/testdom.scm
@@ -0,0 +1,48 @@
+﻿;;使方向键控制按钮
+(define (addpx d a n)
+  (define (getpx str)
+    (string->number str))
+  (dom-set d a (string-append
+              (number->string (+ (getpx (dom-get d a)) n))
+              "px")))
+
+(define d (dom-get (getElementById document "run") 'style))
+(dom-set d 'position "absolute")
+(dom-set d 'left "100px")
+(dom-set d 'top 0)
+(dom-set-event document 'onkeydown
+  (lambda (event)
+    (define KEY_L 37)
+    (define KEY_U 38)
+    (define KEY_R 39)
+    (define KEY_D 40)
+    (define key (dom-get event 'keyCode))
+    (cond ((= key KEY_U) (addpx d 'top -10))
+          ((= key KEY_D) (addpx d 'top +10))
+          ((= key KEY_L) (addpx d 'left -10))
+          ((= key KEY_R) (addpx d 'left +10)))))
+
+
+;;让按钮跟随鼠标，按esc键切换暂停/开始
+(define (topxstr num)
+  (string-append (number->string num) "px"))
+
+(define e (getElementById document "run"))
+(define d (dom-get e 'style))
+(dom-set d 'position "absolute")
+(dom-set d 'left "100px")
+(dom-set d 'top "200px")
+
+(define pause #f)
+
+(dom-set-event document 'onmousemove
+  (lambda (event)
+    (if (not pause)
+      (begin
+        (dom-set d 'left (topxstr (dom-get event 'clientX)))
+        (dom-set d 'top (topxstr (dom-get event 'clientY)))))))
+
+(dom-set-event document 'onkeydown
+  (lambda (event)
+    (if (= (dom-get event 'keyCode) 27)
+        (set! pause (not pause)))))
